0b717f6f3bb33db366cb9cd567d2fb7044dcc239
Caio Costa Cordeiro <caio.c.cordeiro@outlook.com>
2019-10-31 21:01:23 -0300

    Adição da Classe Lote e Clase Medicamento

---
+++ projMedicamento/Lote.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMedicamento
+{
+    class Lote
+    {
+        private int id;
+        private int qtde;
+        private DateTime venc;
+
+        public int Id { get => id; set => id = value; }
+        public int Qtde { get => qtde; set => qtde = value; }
+        public DateTime Venc { get => venc; set => venc = value; }
+
+        public Lote()
+        {
+        }
+
+        public Lote(int id, int qtde, DateTime venc)
+        {
+            this.id = id;
+            this.qtde = qtde;
+            this.venc = venc;
+        }
+
+        public string toString()
+        {
+            return id + "-" + qtde + "-" + venc;
+        }
+    }
+}
---
+++ projMedicamento/Medicamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projMedicamento
+{
+    class Medicamento
+    {
+        private int id;
+        private string nome;
+        private string laboratorio;
+        private Queue<Lote> lotes;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Laboratorio { get => laboratorio; set => laboratorio = value; }
+        internal Queue<Lote> Lotes { get => lotes; set => lotes = value; }
+
+        public Medicamento()
+        {
+        }
+
+        public Medicamento(int id, string nome, string laboratorio, Queue<Lote> lotes)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.laboratorio = laboratorio;
+            this.lotes = lotes;
+        }
+
+        public int qtdeDisponivel()
+        {
+            int qt = 0;
+
+            foreach (Lote a in lotes)
+            {
+                qt += a.Qtde;
+            }
+
+            return qt;
+        }
+
+        public void comprar(Lote lote)
+        {
+            lotes.Enqueue(lote);
+        }
+
+        public bool vender(int qtde)
+        {
+            if(qtde <= qtdeDisponivel())
+            {
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
